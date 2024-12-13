6df9b843589e116b22cea47a65e3b0162c33f365
Nahuel <nahulem@gmail.com>
2024-12-13 20:52:12 -0300

    feat (developement): add showMenu and handle reservation methods

--- src/main/java/example/hotel/Main.java
+++ src/main/java/example/hotel/Main.java
@@ -1,5 +1,7 @@
 package example.hotel;
 
+import java.util.Scanner;
+
 public class Main {
 
     static String[][] hotels = {
@@ -16,5 +18,47 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Bienvenido al HotelApp");
+        showMenu();
     }
+
+    public static void showMenu() {
+        Scanner scanner = new Scanner(System.in);
+        int option;
+        boolean exit = false;
+
+        while (!exit) {
+            System.out.println("\nMenú principal:");
+            System.out.println("1. Crear una reserva");
+            System.out.println("2. Actualizar una reserva");
+            System.out.println("3. Cancelar una reserva");
+            System.out.println("4. Salir");
+
+            option = scanner.nextInt();
+            switch (option) {
+                case 1 -> createReservation();
+                case 2 -> updateReservation();
+                case 3 -> cancelReservation();
+                case 4 -> {
+                    System.out.println("Gracias por utilizar HotelApp. Hasta la próxima! :)");
+                    scanner.close();
+                    exit = true;
+                }
+                default -> System.out.println("Opción inválida. Por favor, elija una opción válida.");
+            }
+        }
+    }
+
+    public static void createReservation() {
+        System.out.println("Crear una reserva");
+    }
+
+    public static void updateReservation() {
+        System.out.println("Actualizar una reserva");
+    }
+
+    public static void cancelReservation() {
+        System.out.println("Cancelar una reserva");
+    }
+
+
 }
